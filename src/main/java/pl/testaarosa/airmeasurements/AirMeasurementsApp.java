40c84ce2c52ccc589e40f7c7e3c5dea532835bcd
Maciej <maciek@testaarosa.pl>
2018-10-30 12:45:17 +0100

    ready Tomcat deploy postgresql

--- src/main/java/pl/testaarosa/airmeasurements/AirMeasurementsApp.java
+++ src/main/java/pl/testaarosa/airmeasurements/AirMeasurementsApp.java
@@ -1,31 +1,31 @@
-package pl.testaarosa.airmeasurements;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
-
-@SpringBootApplication
-@EnableScheduling
-@EnableAsync
-public class AirMeasurementsApp {
-
-    public static void main(String[] args) {
-        SpringApplication.run(AirMeasurementsApp.class, args);
-    }
-
-    @Bean
-    public Executor asyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(40);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("DataBase-> ");
-        executor.initialize();
-        return executor;
-    }
-}
+//package pl.testaarosa.airmeasurements;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//import java.util.concurrent.Executor;
+//
+//@SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
+//public class AirMeasurementsApp {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(AirMeasurementsApp.class, args);
+//    }
+//
+//    @Bean
+//    public Executor asyncExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(10);
+//        executor.setMaxPoolSize(40);
+//        executor.setQueueCapacity(500);
+//        executor.setThreadNamePrefix("DataBase-> ");
+//        executor.initialize();
+//        return executor;
+//    }
+//}
--- src/main/java/pl/testaarosa/airmeasurements/scheduler/AddMesurementsScheduler.java
+++ src/main/java/pl/testaarosa/airmeasurements/scheduler/AddMesurementsScheduler.java
@@ -10,8 +10,6 @@ import pl.testaarosa.airmeasurements.services.AddMeasurementsService;
 import pl.testaarosa.airmeasurements.services.EmailNotifierService;
 
 import java.time.LocalDateTime;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 import static pl.testaarosa.airmeasurements.services.ConsolerData.*;
 
@@ -29,7 +27,7 @@ public class AddMesurementsScheduler {
     private EmailNotifierService emailNotifierService;
 
 
-//    @Scheduled()
+    @Scheduled(cron = "0 01 13,21,5 * * *")
     public void addAllMeasurementsSchedule() {
         LOGGER.info(ANSI_PURPLE + "Add measurements is starting and send e-mail notify" + ANSI_RESET);
         emailNotifierService.sendEmailBeforAddMeasuremetns("first");
@@ -68,54 +66,4 @@ public class AddMesurementsScheduler {
         Thread t2 = new Thread(secendTry);
         t2.start();
     }
-
-//    @Scheduled(cron = "0 0/5 * * * *")
-//    public void testor() {
-//        Runnable secendTry = () -> {
-//            int count = 0;
-//            while (whiteFlag) {
-//                if (LocalDateTime.now().isAfter(timeStamp.plusSeconds(30))) {
-//                    LOGGER.info(ANSI_BLUE + "INSIDE WHILE LOOP COUNTER-> " + count + ANSI_RESET);
-//                    count++;
-//                    sleep(1500);
-//                    if (count > 15) {
-//                        LOGGER.info(ANSI_YELLOW + "JOB FINISHED. COUNTER IS: " + count + ANSI_RESET);
-//                        whiteFlag = false;
-//                    } else if (count == new Random().nextInt(15)) {
-//                        LOGGER.error(ANSI_RED + "ERROR THROWN FROM THREAD LOOP COUNTER -> " + count + ANSI_RESET);
-//                        throw new TransactionException("ERROR THROWN FROM THREAD LOOP");
-//                    }
-//
-//                }
-//            }
-//        };
-//        Thread t2 = new Thread(secendTry);
-//
-//        LOGGER.info(ANSI_PURPLE + "Add measurements is starting and send email notify" + ANSI_RESET);
-//        emailNotifierService.sendEmailBeforAddMeasuremetns();
-//        for (int i = 0; i < 11; i++) {
-//            LOGGER.info(ANSI_CYAN + "Loop number: -> " + i + ANSI_RESET);
-//            sleep(500);
-//            if (i == 10) {
-//                LOGGER.warn(ANSI_RED + "CIRICAL LOOP SO THROW EXCEPTION AND START THREAD, SAVE DATE SEND MAIL ERROR" + ANSI_RESET);
-//                whiteFlag = true;
-//                timeStamp = LocalDateTime.now();
-//                t2.start();
-//            }
-//        }
-//    }
-//
-//    private int radomStationId() {
-//        int randomId[] = new int[]{117, 129, 114, 52, 11, 920, 530, 870, 10554};
-//        return randomId[new Random().nextInt(randomId.length)];
-//    }
-//
-//    private void sleep(int ms) {
-//        try {
-//            Thread.sleep(ms);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 }
